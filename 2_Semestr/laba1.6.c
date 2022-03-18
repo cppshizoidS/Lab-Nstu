6d6649ddd0cfcf780b0f6dcc298dc1ce208dff32
Cppshizoid <alex.krivoschecov@gmail.com>
2022-03-18 23:34:21 +0700

    Update laba1.6.c

--- 2_Semestr/laba1.6.c
+++ 2_Semestr/laba1.6.c
@@ -1,4 +1,4 @@
-##include <stdio.h>
+#include <stdio.h>
 static int vars[] = {12, 8, 32, 123};
 int main() {
     printf("\n\t\tИсходные данные:\n\tvars[0]=%d, vars[1]=%d, vars[2]=0%o, vars[3]=0%o", vars[0], vars[1], vars[2], vars[3]);
